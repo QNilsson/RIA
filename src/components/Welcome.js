820c18f7e83d5ba0d8842bb92d7f80c8624a2692
Quinn <quinn.nilsson@gmail.com>
2020-10-28 15:10:17 -0600

    more with login

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,19 +1,24 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import Image from '../images/image.jpg';
 import {makeStyles} from '@material-ui/core';
+import  {LogContext }from '../contexts/LogContext';
 
 const useStyles = makeStyles (() => ({
   root: {
-   color:'red'
+    color: 'red',
   },
 }));
+
 const Welcome = () => {
   const classes = useStyles ();
+
+  const {isLog} = useContext (LogContext);
   return (
     <div className={classes.root}>
-      <h1>Welcome to Best Chicken & Fish Recipes!</h1>
-      <h2>Please Log In to Continue</h2>
-      <img src={Image} alt="fish chicken" />
+      {isLog
+        ? <h1>Welcome to Best Chicken & Fish Recipes!</h1>
+        : <h2>Please Log In to Continue</h2>}
+     
     </div>
   );
 };
